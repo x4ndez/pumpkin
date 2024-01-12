b962fbb00f0189970eacf307a89b4414410acc50
x.ande <ananfro@live.com>
2024-01-12 14:03:26 +1100

    create posts now updates the parent flatlist with setState

--- client/screens/components/CreatePost.js
+++ client/screens/components/CreatePost.js
@@ -1,35 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button, Linking, Modal, Alert, Switch, FlatList, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Linking, Modal, Alert, Switch, FlatList, TouchableOpacity, Image, Pressable } from 'react-native';
 import { useState, useEffect, useContext } from 'react';
 import { UserContext } from '../context';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { style } from '../styles';
-import { addPost } from '../../helpers';
+import { addPost, getAllPosts } from '../../helpers';
 
-export default function CreatePost() {
+export default function CreatePost({ setPostsData }) {
 
-  const [postInp, setPostInp] = useState();
+  // console.log(postSubmit, setPostSubmit)
+  // THIS COMPONENT REQUIRES:
+  // setPostsData useStates passed in as props so that the parent can update the flatList.
+
+  const [postInp, setPostInp] = useState('');
   const { currentUser } = useContext(UserContext);
+
+  const onPostSubmit = async () => {
+
+    await addPost(
+      currentUser.id,
+      postInp);
+    setPostInp('');
+
+    // update the posts in the parent flatlist
+    const posts = await getAllPosts();
+    setPostsData(posts);
+  }
 
   return (
 
     <View>
 
-      <Text style={[style.genericText]}>CreatePost</Text>
+      <Text style={[style.genericText]}>Create Post</Text>
       <TextInput
         style={style.textInput}
         multiline={true}
+        value={postInp}
         onChangeText={setPostInp}
         placeholder='qual e`'
       />
+
       <Button
         title='Send'
-        onPress={() => addPost(
-          currentUser.id,
-          postInp)}
+        onPress={onPostSubmit}
       />
 
-    </View>
+    </View >
   )
 
 }
