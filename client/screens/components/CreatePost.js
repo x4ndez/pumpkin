49732d701b8e2c8bd4e7532421b17fd8b10b1ae7
x.ande <ananfro@live.com>
2024-01-13 14:16:53 +1100

    create post styling

--- client/screens/components/CreatePost.js
+++ client/screens/components/CreatePost.js
@@ -5,19 +5,20 @@ import { UserContext } from '../context';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { style } from '../styles';
 import { addPost, getAllPosts } from '../../helpers';
+import SubHeading from './SubHeading';
 
 export default function CreatePost({ setPostsData }) {
 
-  // console.log(postSubmit, setPostSubmit)
   // THIS COMPONENT REQUIRES:
   // setPostsData useStates passed in as props so that the parent can update the flatList.
 
   const [postInp, setPostInp] = useState('');
   const { currentUser } = useContext(UserContext);
+  const navigation = useNavigation();
 
   const onPostSubmit = async () => {
 
-    await addPost(
+    const addedPost = await addPost(
       currentUser.id,
       postInp);
     setPostInp('');
@@ -25,25 +26,53 @@ export default function CreatePost({ setPostsData }) {
     // update the posts in the parent flatlist
     const posts = await getAllPosts();
     setPostsData(posts);
+
+    navigation.navigate('Post', {
+      postId: addedPost.id
+    })
   }
 
   return (
 
     <View>
 
-      <Text style={[style.genericText]}>Create Post</Text>
-      <TextInput
-        style={style.textInput}
-        multiline={true}
-        value={postInp}
-        onChangeText={setPostInp}
-        placeholder='qual e`'
-      />
+      <View style={style.createPostContainer}>
 
-      <Button
-        title='Send'
-        onPress={onPostSubmit}
-      />
+        <SubHeading
+          props={{
+            title: 'Make an Announcement',
+          }}
+        />
+
+        <View style={{ position: 'relative' }}>
+
+          <TextInput
+            style={style.createPostTextInput}
+            multiline={true}
+            value={postInp}
+            onChangeText={setPostInp}
+            placeholder="What's on your mind?"
+          />
+
+          <Pressable
+
+            onPress={onPostSubmit}
+            style={{
+              position: 'absolute',
+              right: 0,
+              display: 'flex',
+              alignItems: 'center',
+              paddingTop: 10,
+              paddingRight: 10,
+            }}
+          ><Image
+              source={require('../../assets/send_full.png')}
+              style={style.commentSendImg}
+            /></Pressable>
+
+        </View>
+
+      </View>
 
     </View >
   )
