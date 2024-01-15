dd83072a1ba24a67f198590ae7f74987dd520b17
x.ande <ananfro@live.com>
2024-01-15 18:59:38 +1100

    AddMemberBtn modal styling

--- client/screens/components/AddMemberBtn.js
+++ client/screens/components/AddMemberBtn.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button, Linking, Modal, Alert } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Linking, Modal, Alert, Pressable } from 'react-native';
 import { useState, useEffect } from 'react';
 import { localhostUrl } from '../../config';
+import { style } from '../styles';
+import SubHeading from './SubHeading';
 
 export default function AddMemberBtn() {
 
@@ -40,29 +42,130 @@ export default function AddMemberBtn() {
       <Modal
         visible={modalVisible}
         animationType='fade'
+        transparent={true}
       >
-        <View>
-          <Button
-            title='X'
-            onPress={() => {
-              setModalVisible(false);
-            }}
-          />
-          <Text>Enter the name and email of the new member below...</Text>
-          <TextInput
-            style={styles.textInput}
-            onChangeText={onChangeNameInp}
-            placeholder='Name'
-          />
-          <TextInput
-            style={styles.textInput}
-            onChangeText={onChangeEmailInp}
-            placeholder='Email'
-          />
-          <Button
-            title='Add Member'
-            onPress={addMemberPress}
-          />
+
+        <Pressable style={{
+          backgroundColor: 'black',
+          opacity: 0.85,
+          height: '100%',
+          width: '100%',
+          position: 'absolute',
+        }}
+          onPress={() => setModalVisible(false)}
+        ></Pressable>
+
+        <View style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 10,
+          zIndex: 5,
+          marginTop: 50,
+        }}>
+
+          <View style={{
+            backgroundColor: '#212121',
+            width: '80%',
+            paddingTop: 10,
+            paddingBottom: 10,
+            paddingLeft: 20,
+            paddingRight: 20,
+            borderRadius: 20,
+            // borderWidth: 0.5,
+            // borderColor: 'grey',
+          }}>
+            {/* <Button
+              title='X'
+              onPress={() => {
+                setModalVisible(false);
+              }}
+            /> */}
+
+            <SubHeading
+              props={{
+                title: 'New Member'
+              }}
+            />
+
+            <Text style={style.subText}>Enter the name and email of the new member below...</Text>
+
+            <View style={{
+              display: 'flex',
+              flexDirection: 'row',
+              backgroundColor: '#313131',
+              alignItems: 'center',
+              width: '100%',
+              paddingLeft: 15,
+              borderRadius: 20,
+              marginBottom: 10,
+              marginTop: 10,
+            }}>
+              <View style={{
+                width: '20%',
+              }}>
+                <Text style={style.genericText}>Name</Text>
+              </View>
+              <View style={{
+                width: '80%',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'flex-end',
+                paddingRight: 15,
+              }}>
+                <TextInput
+                  style={styles.textInput}
+                  onChangeText={onChangeNameInp}
+                  textAlign='right'
+                  placeholder='Tom Riddle'
+                  placeholderTextColor='#757575'
+                />
+              </View>
+            </View>
+
+            <View style={{
+              display: 'flex',
+              flexDirection: 'row',
+              backgroundColor: '#313131',
+              alignItems: 'center',
+              width: '100%',
+              paddingLeft: 15,
+              borderRadius: 20,
+            }}>
+              <View style={{
+                width: '20%',
+              }}>
+                <Text style={style.genericText}>Email</Text>
+              </View>
+              <View style={{
+                width: '80%',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'flex-end',
+                paddingRight: 15,
+              }}>
+                <TextInput
+                  style={styles.textInput}
+                  onChangeText={onChangeEmailInp}
+                  textAlign='right'
+                  placeholder='tomriddle@hogwarts.com.uk'
+                  placeholderTextColor='#757575'
+                />
+              </View>
+            </View>
+
+            <View style={{
+              marginTop: 20,
+              marginBottom: 10,
+            }}>
+              <Button
+                title='Add Member'
+                onPress={addMemberPress}
+              />
+            </View>
+          </View>
+
         </View>
       </Modal>
 
@@ -78,22 +181,7 @@ export default function AddMemberBtn() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   textInput: {
-    backgroundColor: 'white',
-    borderColor: 'grey',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingTop: 5,
-    paddingBottom: 5,
-    marginBottom: 5,
-    width: '40%',
-  }
+    color: 'white',
+  },
 });
